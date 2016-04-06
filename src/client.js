6fc172ac2baab65db425b9679e11995beb4b69eb
Lily Cartelet <flowercartelet@gmail.com>
2016-04-06 20:31:05 +0900

    :art: Add a react-intl

--- src/client.js
+++ src/client.js
@@ -1,17 +1,28 @@
 import './polyfill';
-import 'moment/locale/ja';
+import 'intl';
+import 'intl/locale-data/jsonp/ja';
 import 'whatwg-fetch';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactIntl, { IntlProvider } from 'react-intl';
+import jaLocaleData from 'react-intl/locale-data/ja';
 import { browserHistory as history, match, Router } from 'react-router';
 import routes from './routes';
+import getRoot from './utils/getRoot';
+
+const root = getRoot();
+const locale = root.lang || en;
+
+ReactIntl.addLocaleData(jaLocaleData);
 
 match({ history, routes }, function(error, redirectLocation, renderProps) {
   if (error instanceof Error) {
     throw error;
   }
   ReactDOM.render(
-    <Router {...renderProps}/>,
+    <IntlProvider locale={locale}>
+      <Router {...renderProps}/>
+    </IntlProvider>,
     document.getElementById('app')
   );
 });
